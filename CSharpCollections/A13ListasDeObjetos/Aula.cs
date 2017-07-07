543398fc234b9528d36ed02a58106bf41111c382
marcelooliveira <mclricardo@gmail.com>
2017-07-07 19:35:13 -0300

    AULA 02 Listas de objetos
    AULA 02 Listas de objetos
    https://cursos.alura.com.br/course/java-collections/task/14169

---
+++ CSharpCollections/A13ListasDeObjetos/Aula.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A13ListasDeObjetos
+{
+    class Aula : IComparable
+    {
+        private string titulo;
+        private int tempo;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public int Tempo
+        {
+            get { return tempo; }
+            set { tempo = value; }
+        }
+
+        public Aula(string titulo, int tempo)
+        {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+
+        public override string ToString()
+        {
+            return $"[Aula: {this.titulo}, {this.tempo} minutos]";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Aula that = obj as Aula;
+            return this.titulo.CompareTo(that.titulo);
+        }
+    }
+}
--- CSharpCollections/A13ListasDeObjetos/Program.cs
+++ CSharpCollections/A13ListasDeObjetos/Program.cs
@@ -10,6 +10,45 @@ namespace A13ListasDeObjetos
     {
         static void Main(string[] args)
         {
+            Aula a1 = new Aula("Revisitando listas", 21);
+            Aula a2 = new Aula("Listas de objetos", 15);
+            Aula a3 = new Aula("Relacionamento de listas e objetos", 13);
+
+            List<Aula> aulas = new List<Aula>();
+            aulas.Add(a1);
+            aulas.Add(a2);
+            aulas.Add(a3);
+            //a próxima linha gera um erro de compilação!
+            //aulas.Add("Equals e HashCode");
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula); //não exibe detalhes da aula, pois precisamos implementar ToString()
+            }
+
+            //Após implementarmos ToString() em Aula, a linha acima exibe corretamente a aula
+
+            aulas.Sort();
+            ////Exceção Sem Tratamento: System.InvalidOperationException: 
+            //Falha ao comparar dois elementos na matriz. --->System.ArgumentException: 
+            //Pelo menos um objeto deve implementar IComparable.
+
+           ImprimeAulas(aulas);
+
+            //vamos ordenar de outro jeito agora
+            aulas.Sort((x, y) => x.Tempo.CompareTo(y.Tempo));
+
+            ImprimeAulas(aulas);
+
+        }
+
+        private static void ImprimeAulas(List<Aula> aulas)
+        {
+            Console.WriteLine();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula); //não exibe detalhes da aula, pois precisamos implementar ToString()
+            }
         }
     }
 }
