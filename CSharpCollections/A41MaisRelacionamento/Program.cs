fdff96ed5d44fe666a5f17390ba2e12f833c32e3
marcelooliveira <mclricardo@gmail.com>
2017-07-10 18:56:07 -0300

    De acordo com Java Collections: AULA 04 - Mais práticas com relacionamentos
    De acordo com Java Collections: AULA 04 - Mais práticas com
    relacionamentos

--- CSharpCollections/A41MaisRelacionamento/Program.cs
+++ CSharpCollections/A41MaisRelacionamento/Program.cs
@@ -10,6 +10,34 @@ namespace A41MaisRelacionamento
     {
         static void Main(string[] args)
         {
+            Curso csharpColecoes = new Curso("Dominando as coleções do C#", "Marcelo Oliveira");
+
+            //Adicionando várias aulas
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            ////Imprimindo várias aulas
+            //IList<Aula> aulasImutaveis = csharpColecoes.Aulas;
+            //ImprimeAulas(aulasImutaveis);
+
+            //Copiando uma ReadonOnlyCollection para uma Lista e ordenando
+            List<Aula> aulas = new List<Aula>(csharpColecoes.Aulas);
+            aulas.Sort();
+            ImprimeAulas(aulas);
+
+            Console.WriteLine(csharpColecoes.TempoTotal);
+
+            Console.WriteLine(csharpColecoes);
+        }
+
+        private static void ImprimeAulas(IList<Aula> aulas)
+        {
+            Console.WriteLine();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
         }
     }
 }
