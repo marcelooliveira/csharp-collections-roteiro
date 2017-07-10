7c13cfbe4ca38144c4e20807b9921171ed93b85a
marcelooliveira <mclricardo@gmail.com>
2017-07-10 17:39:47 -0300

    Vídeo aula 3 de Java Collections: Relacionamentos com coleções
    Vídeo aula 3 de Java Collections: Relacionamentos com coleções

---
+++ CSharpCollections/A31Relacionamentos/Curso.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A31Relacionamentos
+{
+    class Curso
+    {
+        private string nome;
+        private string instrutor;
+        //A aula começa com List, depois muda para IList e ReadOnlyCollection
+        //private List<Aula> aulas = new List<Aula>();
+        private IList<Aula> aulas = new List<Aula>();
+
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        public string Instrutor
+        {
+            get { return instrutor; }
+        }
+
+        //A aula começa com List, depois muda para IList e ReadOnlyCollection
+        //public List<Aula> Aulas
+        //{
+        //    get { return aulas; }
+        //}
+
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+        }
+
+        public void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+    }
+}
--- CSharpCollections/A31Relacionamentos/Program.cs
+++ CSharpCollections/A31Relacionamentos/Program.cs
@@ -10,6 +10,48 @@ namespace A31Relacionamentos
     {
         static void Main(string[] args)
         {
+            Curso csharpColecoes = new Curso("Dominando as coleções do C#", "Marcelo Oliveira");
+
+            ////Ensinando a acessar propriedade Aulas de um objeto Curso
+            //List<Aula> aulas = csharpColecoes.Aulas;
+            //Console.WriteLine();
+            //foreach (var aula in aulas)
+            //{
+            //    Console.WriteLine(aula);
+            //}
+
+            ////Ensinando que aulas e csharpColecoes.Aulas são o mesmo objeto
+            //aulas.Add(new Aula("Trabalhando com Listas", 21));
+            //ImprimeAulas(aulas);
+            //ImprimeAulas(csharpColecoes.Aulas); //mostra o mesmo que o anterior, ou seja, é o mesmo objeto!
+            //Console.WriteLine(aulas == csharpColecoes.Aulas); //São iguais mesmo? Vamos verificar!
+
+            ////Ensinando a encapsular e mover método de adição de elemento na classe Curso
+            //csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            //ImprimeAulas(csharpColecoes.Aulas);
+
+            ////Ensinando que os dois métodos fazem a mesma coisa
+            //csharpColecoes.Aulas.Add(new Aula("Trabalhando com Listas", 21));
+            //csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            //ImprimeAulas(csharpColecoes.Aulas);
+
+            
+            ////Ensinando a não permitir adição de aula fora da classe Curso
+            //csharpColecoes.Aulas.Add(new Aula("Trabalhando com Listas", 21));
+            //A linha acima produz: Exceção Sem Tratamento: System.NotSupportedException: Coleção é somente leitura.
+            //mas a linha abaixo funciona!
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            ImprimeAulas(csharpColecoes.Aulas);
+        }
+
+        //private static void ImprimeAulas(List<Aula> aulas)
+        private static void ImprimeAulas(IList<Aula> aulas)
+        {
+            Console.WriteLine();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
         }
     }
 }
