c7b458e1d6d725a4736a01c3936682c2ffd19ea5
marcelooliveira <mclricardo@gmail.com>
2017-07-11 11:19:10 -0300

    AULA 06 - Aplicando o Set no modelo

---
+++ CSharpCollections/A61SetNoModelo/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A61SetNoModelo
+{
+    public class Aluno
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        private int numeroMatricula;
+        public int NumeroMatricula
+        {
+            get { return numeroMatricula; }
+        }
+
+        public Aluno(String nome, int numeroMatricula)
+        {
+            this.nome = nome;
+            this.numeroMatricula = numeroMatricula;
+        }
+
+        public override string ToString()
+        {
+            return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+        }
+    }
+}
--- CSharpCollections/A61SetNoModelo/Program.cs
+++ CSharpCollections/A61SetNoModelo/Program.cs
@@ -10,6 +10,24 @@ namespace A61SetNoModelo
     {
         static void Main(string[] args)
         {
+            Curso csharpColecoes = new Curso("Dominando as coleções do C#", "Marcelo Oliveira");
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+            Console.WriteLine("Todos os alunos matriculados: ");
+            csharpColecoes.Alunos.ToList().ForEach(aluno => {
+                Console.WriteLine(aluno);
+            });
         }
     }
 }
