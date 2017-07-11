af4832bdc1a91c7df49480f4318bf35850052327
marcelooliveira <mclricardo@gmail.com>
2017-07-11 18:15:59 -0300

    AULA 09 - Qual Collection usar
    AULA 09 - Qual Collection usar

--- CSharpCollections/QualCollectionUsar/Program.cs
+++ CSharpCollections/QualCollectionUsar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +8,21 @@ using System.Threading.Tasks;
 namespace QualCollectionUsar
 {
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            Collection<Aluno> alunos = new Collection<Aluno>();
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+            alunos.Add(a1);
+            alunos.Add(a2);
+            alunos.Add(a3);
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
         }
     }
 }
