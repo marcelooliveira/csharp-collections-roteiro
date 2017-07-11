d84d671e602054ad568af99faea4916c92c00600
marcelooliveira <mclricardo@gmail.com>
2017-07-11 18:02:51 -0300

    AULA 08 - Outros sets e iterators
    AULA 08 - Outros sets e iterators

---
+++ CSharpCollections/A81OutrosSetsEIterators/Curso.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A81OutrosSetsEIterators
+{
+    class Curso
+    {
+        private string nome;
+        private string instrutor;
+        //A aula começa com List, depois muda para IList e ReadOnlyCollection
+        //private List<Aula> aulas = new List<Aula>();
+        private IList<Aula> aulas = new List<Aula>();
+        ISet<Aluno> alunos = new HashSet<Aluno>();
+
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        public string Instrutor
+        {
+            get { return instrutor; }
+        }
+
+        internal void Matricula(Aluno aluno)
+        {
+            this.alunos.Add(aluno);
+        }
+
+        //A aula começa com List, depois muda para IList e ReadOnlyCollection
+        //public List<Aula> Aulas
+        //{
+        //    get { return aulas; }
+        //}
+
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+        internal bool EstaMatriculado(Aluno aluno)
+        {
+            return this.alunos.Contains(aluno);
+        }
+
+        //public IList<Aluno> Alunos
+        public ISet<Aluno> Alunos
+        {
+            get
+            {
+                return new HashSet<Aluno>(alunos.ToList());
+            }
+        }
+        
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+        }
+
+        public void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+        //public int TempoTotal
+        //{
+        //    get
+        //    {
+        //        int tempoTotal = 0;
+        //        foreach (var aula in aulas)
+        //        {
+        //            tempoTotal += aula.Tempo;
+        //        }
+
+        //        return tempoTotal;
+        //    }
+        //}
+
+        public int TempoTotal
+        {
+            get
+            {
+                return aulas.Sum(a => a.Tempo);
+            }
+        }
+
+        public override string ToString()
+        {
+            IEnumerable<string> aulasString = aulas.Select(a => a.ToString());
+            return $"[Curso: {nome}, tempo total: {TempoTotal}, aulas: {string.Join(",", aulasString)}]";
+        }
+    }
+}
--- CSharpCollections/A81OutrosSetsEIterators/Program.cs
+++ CSharpCollections/A81OutrosSetsEIterators/Program.cs
@@ -10,6 +10,42 @@ namespace A81OutrosSetsEIterators
     {
         static void Main(string[] args)
         {
+            Curso csharpColecoes = new Curso("Dominando as coleções do C#", "Marcelo Oliveira");
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+            Console.WriteLine("Todos os alunos matriculados: ");
+            ISet<Aluno> alunos = csharpColecoes.Alunos;
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            var enumerador = alunos.GetEnumerator();
+            //enumerador.MoveNext(); //mostrar este método, que retorna true se houve um próximo
+
+            Aluno alunoAtual = enumerador.Current;
+            if (alunoAtual != null)
+            {
+                while (enumerador.MoveNext())
+                {
+                    Console.WriteLine(alunoAtual);
+                }
+            }
+
+            //enumerador.MoveNext();
+            //Aluno depoisDoUltimo = enumerador.Current;
         }
     }
 }
