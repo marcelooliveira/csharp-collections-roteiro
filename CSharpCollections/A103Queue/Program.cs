e46661e5a10314cb91d434090b2824f7cc57db03
marcelooliveira <mclricardo@gmail.com>
2017-07-26 14:02:15 -0300

    Aula 10  Vídeo 3 - Queues (Filas)
    Aula 10  Vídeo 3 - Queues (Filas)

---
+++ CSharpCollections/A103Queue/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A103Queue
+{
+    class Program
+    {
+        static Queue<string> pedagio = new Queue<string>();
+        static void Main(string[] args)
+        {
+
+            //string veiculo = "van";
+            //Console.WriteLine($"Entrou na fila: {veiculo}");
+            //pedagio.Enqueue(veiculo);
+            //foreach (var v in pedagio)
+            //{
+            //    Console.WriteLine(v);
+            //}
+
+            ///<image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
+            //string veiculo = "van";
+            //Enfileirar(veiculo);
+
+            Enfileirar("van");
+            Enfileirar("kombi");
+            ///<image url="$(ProjectDir)\Slides\queue2.png" scale=""/>
+            Enfileirar("guincho");
+            ///<image url="$(ProjectDir)\Slides\queue3.png" scale=""/>
+            Enfileirar("pickup");
+            ///<image url="$(ProjectDir)\Slides\queue4.png" scale=""/>
+
+            Desenfileirar();
+            ///<image url="$(ProjectDir)\Slides\queue5.png" scale=""/>
+            Desenfileirar();
+            ///<image url="$(ProjectDir)\Slides\queue6.png" scale=""/>
+            Desenfileirar();
+            ///<image url="$(ProjectDir)\Slides\queue7.png" scale=""/>
+            Desenfileirar();
+
+        }
+
+        private static void Desenfileirar()
+        {
+            var veiculo = pedagio.Dequeue();
+            Console.WriteLine($"Saiu da fila: {veiculo}");
+            ImprimeFila();
+        }
+
+        private static void Enfileirar(string veiculo)
+        {
+            //Console.WriteLine($"Entrou na fila: {veiculo}");
+            //pedagio.Enqueue(veiculo);
+            //foreach (var v in pedagio)
+            //{
+            //    Console.WriteLine(v);
+            //}
+
+            Console.WriteLine($"Entrou na fila: {veiculo}");
+            pedagio.Enqueue(veiculo);
+            ImprimeFila();
+        }
+
+        private static void ImprimeFila()
+        {
+            Console.WriteLine("FILA:");
+            foreach (var v in pedagio)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine();
+        }
+    }
+}
