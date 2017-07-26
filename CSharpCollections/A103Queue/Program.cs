8a6c582a2e9bf0aebdf02a080ef7c6f484087283
marcelooliveira <mclricardo@gmail.com>
2017-07-26 14:53:16 -0300

    10.3 peek, any
    10.3 peek, any

--- CSharpCollections/A103Queue/Program.cs
+++ CSharpCollections/A103Queue/Program.cs
@@ -20,11 +20,12 @@ namespace A103Queue
             //    Console.WriteLine(v);
             //}
 
-            ///<image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
             //string veiculo = "van";
             //Enfileirar(veiculo);
 
             Enfileirar("van");
+            ///<image url="$(ProjectDir)\Slides\queue1.png" scale=""/>
+
             Enfileirar("kombi");
             ///<image url="$(ProjectDir)\Slides\queue2.png" scale=""/>
             Enfileirar("guincho");
@@ -39,14 +40,21 @@ namespace A103Queue
             Desenfileirar();
             ///<image url="$(ProjectDir)\Slides\queue7.png" scale=""/>
             Desenfileirar();
-
+            Desenfileirar();
         }
 
         private static void Desenfileirar()
         {
-            var veiculo = pedagio.Dequeue();
-            Console.WriteLine($"Saiu da fila: {veiculo}");
-            ImprimeFila();
+            if (pedagio.Any())
+            {
+                if (pedagio.Peek() == "guincho")
+                {
+                    Console.WriteLine("Guincho está esperando no pedágio.");
+                }
+                var veiculo = pedagio.Dequeue();
+                Console.WriteLine($"Saiu da fila: {veiculo}");
+                ImprimeFila();
+            }
         }
 
         private static void Enfileirar(string veiculo)
