97d4d170457ba99b0af26b8c721b7f75056d468e
marcelooliveira <mclricardo@gmail.com>
2017-07-28 17:55:04 -0300

    SortedSet
    SortedSet

--- CSharpCollections/A73SortedSet/Program.cs
+++ CSharpCollections/A73SortedSet/Program.cs
@@ -10,6 +10,28 @@ namespace A73SortedSet
     {
         static void Main(string[] args)
         {
+            ISet<String> alunos = new SortedSet<String>(new ComparadorMinusculo())
+            {
+                "Vanessa Tonini",
+                "Ana Losnak",
+                "Rafael Nercessian",
+                "Priscila Stuani"
+            };
+
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+            alunos.Add("Fabio Gushiken");
+            alunos.Add("FABIO GUSHIKEN");
+            foreach (var a in alunos)
+                Console.WriteLine(a);
+        }
+    }
+
+    internal class ComparadorMinusculo : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
