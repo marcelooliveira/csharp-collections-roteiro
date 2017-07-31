13809d41b67bcd545df0505e8e612a4817d29915
marcelooliveira <mclricardo@gmail.com>
2017-07-31 16:46:54 -0300

    aulas com LINQ
    aulas com LINQ

---
+++ CSharpCollections/A92LINQOrdenacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A92LINQOrdenacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = { "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" };
+
+            Imprimir(meses.Take(3));
+            Imprimir(meses.Skip(3));
+            Imprimir(meses.Reverse());
+        }
+
+        private static void Imprimir(IEnumerable<string> consulta)
+        {
+            foreach (var item in consulta)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
