45dc786892cd6887f0befe80a592f2b3e4a0b21d
marcelooliveira <mclricardo@gmail.com>
2017-07-31 14:42:02 -0300

    A112ConcurrentStackQueueBag
    A112ConcurrentStackQueueBag

---
+++ CSharpCollections/A112ConcurrentStackQueueBag/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A112ConcurrentStackQueueBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new ConcurrentQueue<string>();
+            var stack = new ConcurrentStack<string>();
+            var bag = new ConcurrentBag<string>();
+
+            queue.Enqueue("hello");
+            queue.Enqueue("world");
+
+            var first = string.Empty;
+            if (queue.TryPeek(out first))
+            {
+                //fazer algo com first
+            }
+
+            queue.TryDequeue(out first);
+
+            stack.Push("hello");
+            stack.Push("world");
+            stack.PushRange(new[] { "foo", "bar" }); //4
+
+            stack.TryPeek(out first);
+            stack.TryPop(out first);
+
+            var array = new string[3];
+            stack.TryPopRange(array);
+
+            bag.Add("hello");
+            bag.Add("world");
+        }
+    }
+}
