d7ccea2786c46132ed016912345af4e072c1617a
marcelooliveira <mclricardo@gmail.com>
2017-07-31 13:50:37 -0300

    Multidimensional arrays
    Multidimensional arrays

--- CSharpCollections/A81ArraysMultidimensionais/Program.cs
+++ CSharpCollections/A81ArraysMultidimensionais/Program.cs
@@ -10,6 +10,46 @@ namespace A81ArraysMultidimensionais
     {
         static void Main(string[] args)
         {
+            const int COPA_2014 = 0;
+            const int COPA_2010 = 1;
+            const int COPA_2006 = 2;
+
+            const int CAMPEAO = 0;
+            const int VICE = 1;
+            const int TERCEIRO = 2;
+
+            int[] copas = new int[] { 2014, 2010, 2006 };
+
+            string[,] resultados = new string[3, 3];
+            
+            resultados[COPA_2014, CAMPEAO] = "Alemanha";
+            resultados[COPA_2014, VICE] = "Argentina";
+            resultados[COPA_2014, TERCEIRO] = "Holanda";
+
+            resultados[COPA_2010, CAMPEAO] = "Espanha";
+            resultados[COPA_2010, VICE] = "Holanda";
+            resultados[COPA_2010, TERCEIRO] = "Alemanha";
+
+            resultados[COPA_2006, CAMPEAO] = "Itália";
+            resultados[COPA_2006, VICE] = "França";
+            resultados[COPA_2006, TERCEIRO] = "Alemanha";
+
+            foreach (var selecao in resultados)
+            {
+                Console.WriteLine(selecao);
+            }
+
+            for (int copa = resultados.GetLowerBound(0); copa <= resultados.GetUpperBound(0); copa++)
+            {
+                Console.Write(copas[copa] + " ");
+                for (int posicao = resultados.GetLowerBound(1); posicao <= resultados.GetUpperBound(1); posicao++)
+                {
+                    //Console.WriteLine(resultados[copa, posicao]);
+
+                    Console.Write(resultados[copa, posicao] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ CSharpCollections/A82JaggedArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A82JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int COPA_2014 = 0;
+            const int COPA_2010 = 1;
+            const int COPA_2006 = 2;
+
+            int[] copas = new int[] { 2014, 2010, 2006 };
+
+            string[][] resultados = new string[3][];
+
+            resultados[COPA_2014] = new string[] { "Alemanha", "Argentina", "Holanda" };
+            resultados[COPA_2010] = new string[] { "Espanha", "Holanda", "Alemanha" };
+            resultados[COPA_2006] = new string[] { "Itália", "França", "Alemanha" };
+
+            //mostrar que aqui não são mostrados os detalhes
+            //foreach (var selecao in resultados)
+            //{
+            //    Console.WriteLine(selecao);
+            //}
+
+            for (int i = 0; i < resultados.Length; i++)
+            {
+                var posicoes = resultados[i];
+                Console.Write(copas[i] + " ");
+                foreach (var selecao in posicoes)
+                {
+                    Console.Write(selecao + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
