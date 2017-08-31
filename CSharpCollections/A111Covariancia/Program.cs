6e75744f6fea4ecfceea8bd5dd8a229df8dcdf66
marcelooliveira <mclricardo@gmail.com>
2017-08-31 18:58:33 -0300

    .
    .

--- CSharpCollections/A111Covariancia/Program.cs
+++ CSharpCollections/A111Covariancia/Program.cs
@@ -14,25 +14,22 @@ namespace A111Covariancia
         {
             // cast de string para objeto funciona sem problemas...
             string titulo = "The Simpsons";
-            object obj = titulo;
+            object obj = titulo;    
 
             // ... mas não funciona fazer cast de List<string> para List<object> !
             var familia = new List<string> { "Homer", "Marge", "Bart", "Lisa", "Maggie" };
-            //  List<object> listaObj = familia; // essa linha dá erro!
-
-            // mas cast para IEnumerable<object> funciona...
-            IEnumerable<object> objEnum = familia;
+            //List<object> listaObj = familia; // essa linha dá erro!
 
             //Devido à covariância de arrays, podemos fazer um cast de 
-            //array de strings para array de objects!
+            //array de strings para array de objects! [MAS NÃO DEVEMOS!]
             var familiaArray = new string[] { "Dona Florinda", "Kiko" };
-            object[] objArray = familiaArray;
+            object[] objArray = familiaArray; //[FUNCIONA, MAS NÃO FAÇA ISSO!]
             //MAS ATENÇÃO! objArray que recebeu cast de string[]
             //  não pode receber um int!
             //objArray[0] = 123;
 
-            //PARA SABER MAIS:Array covariance
-            //https://msdn.microsoft.com/en-us/library/aa664572(v=vs.71).aspx
+            // mas cast de List<string> para IEnumerable<object> funciona...
+            IEnumerable<object> enumObj = familia;
         }
     }
 }
